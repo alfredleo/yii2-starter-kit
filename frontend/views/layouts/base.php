691e6060b8a319937ec366614ad64ce310f3fd74
Alfred <ahamidullin@iseck.com>
2019-02-18 16:32:04 +0500

    Fix navbar hide breadcrumbs.

--- frontend/views/layouts/base.php
+++ frontend/views/layouts/base.php
@@ -14,7 +14,7 @@ $this->beginContent('@frontend/views/layouts/_clear.php')
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
+            'class' => 'navbar-inverse navbar-relative-top',
         ],
     ]); ?>
     <?php echo Nav::widget([
